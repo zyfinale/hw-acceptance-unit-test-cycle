8b94e432ea0b4d9dab78dd7c0b73ba7b7e001ede
Yong Zheng <yzheng.hust@gmail.com>
2018-03-26 15:48:15 -0500

    Update routes.rb

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,4 +2,7 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  get '/movies/:id/same_director' => movies#same_director', :as => :same_director
+  
 end
+
